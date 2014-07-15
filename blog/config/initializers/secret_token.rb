b636bfc458bbb93e4093c021b43cc60d3616c431
Kshitij Karthick <kshitij.karthick@gmail.com>
2014-07-15 19:07:45 +0530

    Skeleton Structure of a Blog

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '48e8d70860159ea7bc23f5f90f1b42f348323a84e039ed6e1f848cd3e9102919995cd228abfef5af72639f6b817621832c7f8716d876a64260d2d725bed6c053'
